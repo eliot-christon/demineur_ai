00c024da6e80a92cee9b9b07c0a11afa9e89c177
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2025-06-09 22:56:16 +0200

    small fix test console ui

--- src/tests/test_console_ui.py
+++ src/tests/test_console_ui.py
@@ -1,10 +1,11 @@
 from src.ui.console_ui import ConsoleUI
+from src.core.player import RandomPlayer
 import unittest
 
 class TestConsoleUI(unittest.TestCase):
     def setUp(self):
         from src.core.game_logic import GameLogic
-        self.game_logic = GameLogic(width=5, height=5, mine_count=10, player_name="TestPlayer")
+        self.game_logic = GameLogic(width=9, height=5, mine_count=10, player=RandomPlayer(name="TestPlayer"))
         self.console_ui = ConsoleUI(self.game_logic)
 
     def test_start_game(self):
@@ -19,8 +20,8 @@ class TestConsoleUI(unittest.TestCase):
         # Normally we would capture stdout to verify the output, but here we just check if no exceptions are raised.
         
         # reveal all cells to test display
-        for x in range(5):
-            for y in range(5):
+        for x in range(self.game_logic.board.width):
+            for y in range(self.game_logic.board.height):
                 self.game_logic.board.get_cell(x, y).reveal()
         self.console_ui.display_board()
         
